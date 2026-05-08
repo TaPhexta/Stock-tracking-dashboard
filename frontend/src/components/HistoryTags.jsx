cb4c9b9667359f7093d2b6b2a97e570a31772a23
Oyama Mbatani <oysmbatani@gmail.com>
2026-05-08 20:59:10 +0200

    Add HistoryTags component for displaying history items

--- frontend/src/components/HistoryTags.jsx
+++ frontend/src/components/HistoryTags.jsx
@@ -1,1 +1,17 @@
+function HistoryTags({ history, onSelect, onClear }) {
+  return (
+    <div className="history-tags">
+      {history.map((item) => (
+        <button key={item} onClick={() => onSelect(item)}>
+          {item}
+        </button>
+      ))}
 
+      {history.length > 0 && (
+        <button onClick={onClear}>Clear</button>
+      )}
+    </div>
+  );
+}
+
+export default HistoryTags;
