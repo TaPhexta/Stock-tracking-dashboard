c07c922528a7eac5229b14666edc56722a2dc966
Oyama Mbatani <oysmbatani@gmail.com>
2026-05-08 20:13:15 +0200

    Add function to fetch stock data from Alpha Vantage

--- backend/app/services/alpha_vantage_services.py
+++ backend/app/services/alpha_vantage_services.py
@@ -1,1 +1,9 @@
+import requests
+from app.config import API_KEY
 
+BASE_URL = "https://www.alphavantage.co/query"
+
+def fetch_stock(symbol: str):
+    url = f"{BASE_URL}?function=GLOBAL_QUOTE&symbol={symbol}&apikey={API_KEY}"
+    response = requests.get(url)
+    return response.json()
