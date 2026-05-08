40c8b7aceb1c8732301700cd89a3dd1d47d747b1
Oyama Mbatani <oysmbatani@gmail.com>
2026-05-08 22:02:17 +0200

    Rename function and update API key variable

--- backend/app/services/alpha_vantage_services.py
+++ backend/app/services/alpha_vantage_services.py
@@ -1,9 +1,9 @@
 import requests
-from app.config import API_KEY
+from app.config import ALPHA_VANTAGE_KEY
 
 BASE_URL = "https://www.alphavantage.co/query"
 
-def fetch_stock(symbol: str):
-    url = f"{BASE_URL}?function=GLOBAL_QUOTE&symbol={symbol}&apikey={API_KEY}"
+def get_stock_data(symbol: str):
+    url = f"{BASE_URL}?function=GLOBAL_QUOTE&symbol={symbol}&apikey={ALPHA_VANTAGE_KEY}"
     response = requests.get(url)
     return response.json()
